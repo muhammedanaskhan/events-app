90e845fe558985c10767d2fdb0bba29cccfa60b2
Mohammad anas khan <muhammedanas5186@gmail.com>
2023-02-07 06:49:57 -0800

    three events at home and About us section in respective Page added

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
 
@@ -11,9 +11,36 @@ export default function Home() {
       <header>
         <nav>
           <img></img>
-          
+          <a href='/'>Home</a>
+          <a href='/events'>Events</a>
+          <a href='/about-us'>About Us</a>
         </nav>
       </header>
+
+      <main className={styles.main}>
+        <a href=''></a>
+        <div>
+          <img/>
+          <h2>Events in London</h2>
+          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        </div>
+        <a href=''></a>
+        <div>
+          <img/>
+          <h2>Events in San Francisco</h2>
+          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        </div>
+        <a href=''></a>
+        <div>
+          <img/>
+          <h2>Events in Barcelona</h2>
+          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+
+      </footer>
     </div>
   )
 }
