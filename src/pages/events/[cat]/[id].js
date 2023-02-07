f90bf73031442a82f0b37fa14895d9bbab2dfe91
Mohammad anas khan <muhammedanas5186@gmail.com>
2023-02-07 08:06:01 -0800

    data.json added, events, dynamic page.

---
+++ src/pages/events/[cat]/[id].js
@@ -0,0 +1,7 @@
+const Page = () => {
+    return(
+        <h1>Our Single Event</h1>
+    )
+}
+
+export default Page
--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -1,6 +1,24 @@
 const Page = () => {
     return(
-        <h1>Events Page</h1>
+        
+        <div>
+            <h1>Events Page</h1>
+            <a href=''></a>
+            <div>
+                <img/>
+                <h2>Events in London</h2>
+            </div>
+            <a href=''></a>
+            <div>
+                <img/>
+                <h2>Events in San Francisco</h2>
+            </div>
+            <a href=''></a>
+            <div>
+                <img/>
+                <h2>Events in Barcelona</h2>
+            </div>
+        </div>
     )
 }
 
